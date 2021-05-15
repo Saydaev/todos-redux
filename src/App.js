203a8a63eb0ee078d37047741ebf41cdce08b037
Saydaev <warface-c26@mail.ru>
2021-05-16 00:03:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {loadTodos} from "./actions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const dispatch = useDispatch();
+    const todos = useSelector(state => state.todos);
+    const loading = useSelector(state => state.loading)
+
+    useEffect(() => {
+        dispatch(loadTodos());
+    }, [])
+
+    return (
+        <div className='app'>
+            {loading?
+                <div className='loader'>
+                    Пожалуйста подождите,<br/>
+                    Данные загружаются...
+                </div>
+                : (todos.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <div key={index} className='blok'>
+                                <div className='id'>Задание {item.id}</div>
+                                <div className='title'>{item.body}</div>
+                                <div className='completed'>*</div>
+                            </div>
+                        </div>
+                    )
+                }))}
+        </div>
+    )
+};
 
 export default App;
---
+++ src/actions.js
@@ -0,0 +1,15 @@
+export const loadTodos = () => {
+    return (dispatch) => {
+        dispatch({
+            type: 'start'
+        })
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then((json) => {
+                dispatch({
+                    type: 'load',
+                    payload: json
+                })
+            })
+    }
+}
